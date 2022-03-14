1e945b38509ea033146d7bbb62c4533a96c182fa
Austin Mayer <a.inoue@churadata.okinawa>
2022-03-14 15:00:44 +0900

    initial tic tac toe setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
 function App() {
+  const [boardState, setBoardState] = useState([
+    [
+      { value: "-", key: "a1" },
+      { value: "-", key: "a2" },
+      { value: "-", key: "a3" },
+    ],
+    [
+      { value: "-", key: "b1" },
+      { value: "-", key: "b2" },
+      { value: "-", key: "b3" },
+    ],
+    [
+      { value: "-", key: "c1" },
+      { value: "-", key: "c2" },
+      { value: "-", key: "c3" },
+    ],
+  ]);
+  const [player, setPlayer] = useState("X");
+
+  const handleBoardClick = (rowIndex, columnIndex) => {
+    if (boardState[rowIndex][columnIndex].value === "-") {
+      setBoardState((prev) => {
+        const newBoardState = Array(...prev);
+        newBoardState[rowIndex][columnIndex].value = player;
+        return newBoardState;
+      });
+      setPlayer((prev) => {
+        if (prev === "X") {
+          return "O";
+        }
+        if (prev === "O") {
+          return "X";
+        }
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Current Player: {player}</h1>
+      <tbody>
+        {boardState.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {row.map((column, columnIndex) => (
+              <td
+                onClick={() => handleBoardClick(rowIndex, columnIndex)}
+                key={column.key}
+              >
+                {column.value}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
     </div>
   );
 }
---
+++ src/BoardTitle.js
@@ -0,0 +1,5 @@
+const BoardTitle = (props) => {
+  return <h1>Current Player: {props.player}</h1>;
+};
+
+export default BoardTitle;
