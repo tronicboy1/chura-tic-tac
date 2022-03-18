f7fd9ebaa4de921078a1f5eb5bea92e08ef24552
Austin Mayer <a.inoue@churadata.okinawa>
2022-03-18 17:48:46 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import checkForWinner from "./helpers/check-for-winner";
+import generateNewBoardState from "./helpers/generate-new-board-state";
 import "./index.css";
 
 function App() {
-  const [boardState, setBoardState] = useState([
-    [
-      { value: "-", key: "a1" },
-      { value: "-", key: "a2" },
-      { value: "-", key: "a3" },
-    ],
-    [
-      { value: "-", key: "b1" },
-      { value: "-", key: "b2" },
-      { value: "-", key: "b3" },
-    ],
-    [
-      { value: "-", key: "c1" },
-      { value: "-", key: "c2" },
-      { value: "-", key: "c3" },
-    ],
-  ]);
+  const [dimension, setDimension] = useState(3);
+  const [boardState, setBoardState] = useState(() =>
+    generateNewBoardState(dimension)
+  );
   const [player, setPlayer] = useState("X");
+  const [winner, setWinner] = useState(null);
+
+  useEffect(() => {
+    const winnerCalc = checkForWinner(boardState);
+    if (winnerCalc) {
+      console.log(winnerCalc);
+      setWinner(winnerCalc);
+
+      setTimeout(() => {
+        setBoardState(generateNewBoardState(dimension));
+        setWinner(null);
+      }, 3000);
+    }
+  }, [boardState]);
+
+  useEffect(() => {
+    return () => setBoardState(generateNewBoardState(dimension));
+  }, [dimension]);
 
   const handleBoardClick = (rowIndex, columnIndex) => {
     if (boardState[rowIndex][columnIndex].value === "-") {
       setBoardState((prev) => {
-        const newBoardState = Array(...prev);
+        const newBoardState = prev.map((row) =>
+          row.map((tile) => ({ ...tile }))
+        );
         newBoardState[rowIndex][columnIndex].value = player;
         return newBoardState;
       });
@@ -39,9 +48,18 @@ function App() {
     }
   };
 
+  const handleInput = (event) => {
+    if (Number(event.target.value) > 2) {
+      setDimension(Number(event.target.value));
+    }
+  };
+
   return (
     <div>
-      <h1>Current Player: {player}</h1>
+      <h1>
+        {winner ? `${winner} won the game!!` : `Current Player: ${player}`}
+      </h1>
+      <input type="number" onChange={handleInput} value={dimension} />
       <tbody>
         {boardState.map((row, rowIndex) => (
           <tr key={rowIndex}>
@@ -49,6 +67,7 @@ function App() {
               <td
                 onClick={() => handleBoardClick(rowIndex, columnIndex)}
                 key={column.key}
+                id={column.key}
               >
                 {column.value}
               </td>
